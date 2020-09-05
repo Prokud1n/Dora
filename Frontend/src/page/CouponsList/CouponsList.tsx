eaf849c51b5126e94cef0c5d5db580ab8d79ceae
vprokudin <vprokudin@tsconsulting.com>
2020-09-05 13:27:09 +0300

    добавил CouponsList добавил компонент для отображения SVG

--- Frontend/src/page/CouponsList/CouponsList.tsx
+++ Frontend/src/page/CouponsList/CouponsList.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { SafeAreaView, TextInput, View, Text } from 'react-native';
 import BackStepButton from '../../components/BackStepButton/BackStepButton';
+import SVG from '../../components/SVG/SVG';
 
 import styles from './CouponsList.style';
 
@@ -11,16 +12,19 @@ const CouponsList = () => {
         <SafeAreaView>
             <View style={styles.containerPage}>
                 <BackStepButton />
-                <View style={styles.containerInput}>
+                <View style={styles.containerHeader}>
+                    <SVG svg="settings" height="70%" width="50%" />
                     <TextInput
                         style={styles.input}
                         value={search}
                         onChangeText={setSearch}
                         placeholder="Поиск по талонам"
                     />
+                    <SVG svg="addCoupon" height="70%" width="50%" />
                 </View>
-                <View>
-                    <Text>Пока здесь ничего нет. Нужно добавить талонов.</Text>
+                <View style={styles.containerCoupons}>
+                    <SVG svg="notFoundCoupons" height="70%" width="50%" />
+                    <Text style={styles.notFoundText}>Пока здесь ничего нет. Нужно добавить талонов.</Text>
                 </View>
             </View>
         </SafeAreaView>
